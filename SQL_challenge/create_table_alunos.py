bbd5750ec1bf7981bb4cf9fa7926ac83ae5b88f3
AngelOttoni <angel.m.ottoni@gmail.com>
2024-02-22 18:24:24 -0300

    test

--- SQL_challenge/create_table_alunos.py
+++ SQL_challenge/create_table_alunos.py
@@ -1,11 +1,11 @@
-from db_connection import connect_to_database, close_connection, execute_query
+from db_connection import connect_to_database, close_connection, execute_query, commit_changes
 
 # Connect to the database
 connection = connect_to_database()
 
 # Define the query to create the "alunos" table
 create_table_query = '''
-    CREATE TABLE IF NOT EXISTS alunos_teste (
+    CREATE TABLE IF NOT EXISTS alunos (
         id INTEGER PRIMARY KEY,
         nome VARCHAR(100),
         idade INTEGER,
